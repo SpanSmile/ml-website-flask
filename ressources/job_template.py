77736a3cbe052d8be90c547664f88558d0213dd5
Jesper <116209863+SpanSmile@users.noreply.github.com>
2025-05-11 15:33:17 +0200

    Update

--- ressources/job_template.py
+++ ressources/job_template.py
@@ -1,35 +1,3 @@
-yaml_template1 = """
-apiVersion: batch/v1
-kind: Job
-metadata:
-  name: {{ job_name }}
-  labels:
-    runai/queue: test
-  annotations:
-    gpu-memory: "{{ gpu_memory }}"
-spec:
-  template:
-    spec:
-      schedulerName: kai-scheduler
-      containers:
-        - name: trainer
-          image: {{ image }}
-          command: ["/bin/bash", "-c"]
-          args:
-            - |
-              cp -r /app/* /mnt/nfs && \
-              python /mnt/nfs/train.py
-          volumeMounts:
-            - name: nfs-storage
-              mountPath: /mnt/nfs
-      restartPolicy: Never
-      volumes:
-        - name: nfs-storage
-          nfs:
-            server: {{ nfs_server }}
-            path: {{ nfs_path }}
-            readOnly: false
-"""
 yaml_template = """
 apiVersion: v1
 kind: Pod
